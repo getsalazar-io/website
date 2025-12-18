e11e20883c471dc17fa3fb1c17d756b5dc9ba43b
imolina-sorint <imolina@sorint.com>
2025-12-18 16:57:53 +0100

    Add sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     svg: true,
   },
-  integrations: [astroI18next(), sitemap()],
+  integrations: [astroI18next()],
 });
