5f97a971d02c745b59555d77be5fc4a469fd6f4f
imolina-sorint <imolina@sorint.com>
2026-01-27 15:35:00 +0100

    Add internationalization

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,7 +1,22 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
   defaultLocale: "en",
-  locales: ["en", "es"],
+  locales: ["en", "it"],
+  showDefaultLocale: false,
   namespaces: ["home", "policy", "cookie", "404"],
-  defaultNamespace:"home",
+  defaultNamespace: "home",
+  routing: {
+    prefixDefaultLocale: false,
+  },
+  i18nextServer: {
+    debug: false,
+  },
+  i18nextClient: {
+    debug: false,
+    detection: {
+      order: ['localStorage', 'navigator', 'htmlTag'],
+      caches: ['localStorage'],
+      lookupLocalStorage: 'i18nextLng',
+    },
+  },
 };
